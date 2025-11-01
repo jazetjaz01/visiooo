959658f4b8ba9631a12a5ab0e7c8eee4ad201050
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2025-11-01 17:37:18 +0100

    upload videos2

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-zinc-50 dark:bg-black px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8 text-black dark:text-white">Mes videos ici</h1>
+      <h1 className="text-3xl font-bold mb-8 text-black dark:text-white">Mes videos ici samedi</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {videos.map((video) => (
